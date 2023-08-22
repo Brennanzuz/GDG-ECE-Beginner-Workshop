740ef23b550c04cb900639c09c10fc2e8c9e85d8
Brennanzuz <timingout365@gmail.com>
2023-08-22 20:07:35 +0800

    Add player movement with method 3

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -13,11 +13,15 @@ func _physics_process(delta):
 #		velocity = Vector2.RIGHT
 
 # Method 2
-	var input_x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	var input_y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
-	velocity.x = input_x * SPEED
-	velocity.y = input_y * SPEED
-	velocity.normalized()
+#	var input_x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+#	var input_y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+#	velocity.x = input_x * SPEED
+#	velocity.y = input_y * SPEED
+#	velocity.normalized()
+
+# Method 3
+	var direction = Vector2(Input.get_axis("ui_left", "ui_right"), Input.get_axis("ui_up", "ui_down")).normalized()
+	velocity = direction * SPEED
 	
 	move_and_slide()
 
