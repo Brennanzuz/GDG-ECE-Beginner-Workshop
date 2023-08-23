e441d0a51329e2d69427ce50d37e154d1f55051b
Brennanzuz <timingout365@gmail.com>
2023-08-23 20:20:58 +0800

    Add missile shooting and map base scene

--- scenes/projectiles/missile.gd
+++ scenes/projectiles/missile.gd
@@ -1,6 +1,10 @@
 extends Area2D
 
 @export var damage = 50.0
+const SPEED = 300.0
 
 func _on_lifetime_timeout():
 	queue_free()
+
+func _physics_process(delta):
+	global_position += Vector2.RIGHT.rotated(rotation) * SPEED * delta
